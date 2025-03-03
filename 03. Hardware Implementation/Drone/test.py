1c2be60f5e7f930a8b273727809d60b62e1c86b9
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-03-03 20:43:44 +0530

    Update test.py

--- 03. Hardware Implementation/Drone/test.py
+++ 03. Hardware Implementation/Drone/test.py
@@ -4,6 +4,9 @@ import time
 # Replace with your ESP32's BLE address
 esp32_address = "78:21:84:88:58:a6"
 
+# Replace the Client ID
+clientId = "DronePi"
+
 # Create an instance of the BLE client
 ble_client = BLEClient(esp32_address)
 
@@ -11,16 +14,26 @@ try:
     # Connect to the BLE device
     ble_client.connect()
 
-    # Wait for notifications and print received data
-    print("Waiting for notifications...")
-    while True:
-        if ble_client.wait_for_notifications():
-            data = ble_client.get_received_data()
-            if data:
-                print(f"Data returned by function: {list(data)}")  # Print the data returned by the function
-        else:
-            print("Waiting...")  # Timeout occurred, no notifications received
-        time.sleep(1)  # Add a small delay to avoid busy-waiting
+    # Verification
+    if ble_client.verifyClient(clientId):
+        print("Client verified successfully!")
+        
+        # Wait for notifications and print received data
+        print("Waiting for notifications...")
+
+        for i in range(10):
+            if ble_client.wait_for_notifications():
+                data = ble_client.get_received_data()
+                if data:
+                    print(f"Data returned by function: {list(data)}")  # Print the data returned by the function
+            else:
+                print("Waiting...")  # Timeout occurred, no notifications received
+            
+            time.sleep(1)  # Add a small delay to avoid busy-waiting
+
+    else:
+        print("Client verification failed.")
+        ble_client.disconnect()      
 
 except KeyboardInterrupt:
     print("Exiting...")
