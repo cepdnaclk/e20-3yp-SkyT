6505f2ba539434727cd9dbc63404f19615e8d214
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-04-09 09:12:23 +0530

    Update App.tsx

--- 04. WebApp/Frontend/src/App.tsx
+++ 04. WebApp/Frontend/src/App.tsx
@@ -2,15 +2,22 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Box } from "@mui/material";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <Box width={"100vw"} height={"100vh"} bgcolor={"ghostwhite"}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to={"/home"} />} />
+          {/* Login */}
           <Route path="/login" element={<Login />} />
+
+          {/* Protected Routes */}
+          <Route path="/" element={<Navigate to={"/home"} />} />
           <Route path="/home" element={<Home />} />
+
+          {/* All other routes */}
+          <Route path="/*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </Box>
