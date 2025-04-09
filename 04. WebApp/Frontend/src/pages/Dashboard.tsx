7123504a8f7584cf9d8ab6f6181c6fe6c74bdbb9
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-04-09 10:54:03 +0530

    Create Dashboard.tsx

---
+++ 04. WebApp/Frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import SideBar from "../components/SideBar";
+
+function Dashboard() {
+  return (
+    <Box width={"100%"} height={"100%"} bgcolor={"red"}>
+      <SideBar />
+    </Box>
+  );
+}
+
+export default Dashboard;
