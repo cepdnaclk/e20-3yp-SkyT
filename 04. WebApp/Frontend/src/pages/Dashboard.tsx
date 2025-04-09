5533a23c79d5a206173159a9a7528fac033052f1
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-04-10 00:03:52 +0530

    Update Dashboard.tsx

--- 04. WebApp/Frontend/src/pages/Dashboard.tsx
+++ 04. WebApp/Frontend/src/pages/Dashboard.tsx
@@ -1,10 +1,9 @@
 import { Box } from "@mui/material";
-import SideBar from "../components/SideBar";
 
 function Dashboard() {
   return (
     <Box width={"100%"} height={"100%"} bgcolor={"red"}>
-      <SideBar />
+      Hello
     </Box>
   );
 }
