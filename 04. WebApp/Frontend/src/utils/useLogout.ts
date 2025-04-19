8286a3e29648edc90fafe2409804f23ecc4d8a50
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-04-19 15:01:18 +0530

    Update useLogout.ts

--- 04. WebApp/Frontend/src/utils/useLogout.ts
+++ 04. WebApp/Frontend/src/utils/useLogout.ts
@@ -1,17 +1,20 @@
-import { useNavigate } from "react-router-dom";
 import { useCallback } from "react";
 import { useAuth } from "../context/AuthContext";
+import { ToastAlert } from "../components/ToastAlert";
 
 export default function useLogout() {
-  const navigate = useNavigate();
   const { setUser } = useAuth();
 
   const logout = useCallback(() => {
     console.log("Logging out...");
+
+    ToastAlert({
+      type: "success",
+      title: "Logout successful!",
+    });
     sessionStorage.clear();
     setUser(null);
-    navigate("/login");
-  }, [navigate, setUser]);
+  }, [setUser]);
 
   return logout;
 }
