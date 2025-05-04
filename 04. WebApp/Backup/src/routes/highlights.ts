7e263db8f10ddb8595285df7cc83a6c875020539
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-05-04 10:57:28 +0530

    Remove Backup Folder

--- 04. WebApp/Backup/src/routes/highlights.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getHighlights } from "../controllers/highlights";
-
-const router = express.Router();
-
-router.get("/", getHighlights);
-
-export default router;
--- 04. WebApp/Backup/src/routes/protected.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authenticateUser } from "../middlewares/authMiddleware";
-
-const router = express.Router();
-
-// Protected Route (Only accessible if authenticated)
-router.get("/", authenticateUser);
-
-export default router;
