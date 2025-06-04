692fb59c778fe8c187e09f60214a2fa4a5755094
mathisha034 <150885341+mathisha034@users.noreply.github.com>
2025-06-05 00:37:47 +0530

    uploading older files in local device

---
+++ 08. Drone/src/Testing/Image/2ndMilestone/1_SingleCapture_Mongo.py
@@ -0,0 +1,47 @@
+from picamera import PiCamera
+import time
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import gridfs
+
+
+def save_Image_Mongo(dir):
+	uri = "mongodb+srv://MongoDBUser3YPG11:VYlFFpkaWmqnuPZr@cluster0.78m7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+	# Create a new client and connect to the server
+	client = MongoClient(uri, server_api=ServerApi('1'))
+
+	# Reference to a database
+	db = client["database01"]
+
+	# Initialize GridFS
+	fs = gridfs.GridFS(db, collection="images")
+
+
+	# Open the image file in binary mode and store it in MongoDB
+	with open(dir, "rb") as f:
+		file_id = fs.put(f, filename="/home/raspi/Desktop/Camera/Lot01_2025-03-04_17-04-55.jpg.jpg")
+
+	print(f"Image stored with ID: {file_id}")
+	
+	return file_id
+	
+def captureImage():
+	
+	camera = PiCamera()
+	camera.resolution = (1024, 768)
+	time.sleep(2)
+
+	filename = f"/home/raspi/Desktop/Camera/Lot01_{time.strftime('%Y-%m-%d_%H-%M-%S')}.jpg"
+	camera.capture(filename)
+	print(f"Image saved as {filename}")
+	
+	return filename
+
+
+
+dir = captureImage()
+Id = save_Image_Mongo(dir)
+
+
+
